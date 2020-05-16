7ccb02af6aa63a34e68df18837c518a32eaac89e
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-05-16 16:58:32 -0500

    first commit of vue-slider

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- <img v-bind:src="image1" alt /> -->
+  <div class="container">
+    <transition-group name="fade" tag="div">
+      <div
+        v-for="currentValue in [currentIndex]"
+        v-bind:key="currentValue"
+        class="img-container"
+        v-bind:style="{ backgroundImage: `url('${currentImg}')` }"
+      >
+        <!-- NOTE: you can either use style property and bind the background image or put a div inside the image -->
+        <!-- <img class="myImg" v-bind:src="currentImg" /> -->
+      </div>
+    </transition-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      timer: null,
+      currentIndex: 0,
+      images: [
+        require("../assets/images/artem-bali-682651-unsplash.jpg"),
+        require("../assets/images/brooke-lark-261793-unsplash.jpg"),
+        require("../assets/images/erik-dungan-1079095-unsplash.jpg"),
+        require("../assets/images/ivan-timov-663072-unsplash.jpg")
+      ]
+    };
+  },
+  methods: {
+    next() {
+      this.currentIndex += 1;
+      console.log(
+        this.currentIndex,
+        Math.abs(this.currentIndex) % this.images.length
+      );
+    },
+    prev() {
+      this.currentIndex -= 1;
+    },
+    startSlide() {
+      this.timer = setInterval(this.next, 4000);
+    }
+  },
+  computed: {
+    currentImg: function() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    }
+  },
+  mounted() {
+    this.startSlide();
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.container {
+  width: 100%;
+  height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 2s ease-in;
+  overflow: hidden;
+  visibility: visible;
+  position: absolute;
+  width: 100%;
+  opacity: 1;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.fade-enter,
+.fade-leave-to {
+  visibility: hidden;
+  width: 100%;
+  opacity: 0;
 }
-a {
-  color: #42b983;
+
+.img-container {
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+}
+
+.myImg {
+  width: 100%;
+  height: 100%;
 }
 </style>
